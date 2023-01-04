4f0b5aaafab3fe197641ec01958d2d01f7cc6e1e
Numissa11 <61435050+Numissa11@users.noreply.github.com>
2023-01-04 17:46:45 +0100

    timer bugg fixed

--- pages/components/AudioPlayer.js
+++ pages/components/AudioPlayer.js
@@ -1,37 +1,97 @@
-import React, { useState } from 'react'
-import styles from '../../styles/AudioPlayer.module.css'
+import React, { useState, useRef, useEffect } from 'react'
+import styles from "../../styles/AudioPlayer.module.css";
 import { BsArrowLeftShort } from "react-icons/bs"
 import { BsArrowRightShort } from "react-icons/bs"
 import { FaPlay } from "react-icons/fa"
 import { FaPause } from "react-icons/fa"
 
 const AudioPlayer = () => {
-    const [isPlaying, setIsPlaying] = useState(false);
+  // state
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
 
-    const togglePlayPause = () => {
-        setIsPlaying(!isPlaying); 
+  // references
+  const audioPlayer = useRef();   // reference our audio component
+  const progressBar = useRef();   // reference our progress bar
+  const animationRef = useRef();  // reference the animation
+
+  useEffect(() => {
+    const seconds = Math.floor(audioPlayer.current.duration);
+    setDuration(seconds);
+    progressBar.current.max = seconds;
+  }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState]);
+
+  const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${returnedMinutes}:${returnedSeconds}`;
+  }
+
+  const togglePlayPause = () => {
+    const prevValue = isPlaying;
+    setIsPlaying(!prevValue);
+    if (!prevValue) {
+      audioPlayer.current.play();
+      animationRef.current = requestAnimationFrame(whilePlaying)
+    } else {
+      audioPlayer.current.pause();
+      cancelAnimationFrame(animationRef.current);
     }
+  }
 
-    return (
-        <div className={styles.audioPlayer}>
-            <audio src="https://cdn.simplecast.com/audio/cae8b0eb-d9a9-480d-a652-0defcbe047f4/episodes/af52a99b-88c0-4638-b120-d46e142d06d3/audio/500344fb-2e2b-48af-be86-af6ac341a6da/default_tc.mp3" preload="metadata"></audio>
-            <button className={styles.forwardBackward}> <BsArrowLeftShort /> 30</button>
-            <button onClick={togglePlayPause} className={styles.playPause}>
-                {isPlaying ? <FaPause /> : <FaPlay className={styles.play} />}
-            </button>
-            <button className={styles.forwardBackward}> <BsArrowRightShort /> 30</button>
+  const whilePlaying = () => {
+    progressBar.current.value = audioPlayer.current.currentTime;
+    changePlayerCurrentTime();
+    animationRef.current = requestAnimationFrame(whilePlaying);
+  }
 
-            {/* current time */}
-            <div className={styles.currentTime}>0:00</div>
+  const changeRange = () => {
+    audioPlayer.current.currentTime = progressBar.current.value;
+    changePlayerCurrentTime();
+  }
 
-            {/* progress bar */}
-            <div>
-                <input type='range' className={styles.progressBar} />
-            </div>
+  const changePlayerCurrentTime = () => {
+    progressBar.current.style.setProperty('--seek-before-width', `${progressBar.current.value / duration * 100}%`)
+    setCurrentTime(progressBar.current.value);
+  }
 
-            {/* duration */}
-            <div className={styles.duration} >2:49</div>
-        </div>
-    )
+  const backFive = () => {
+    console.log('back', progressBar.current.value)
+
+    progressBar.current.value = Number(progressBar.current.value) - 5;
+    changeRange();
+  }
+
+  const forwardFive = () => {
+    console.log('forward', progressBar.current.value)
+    progressBar.current.value = Number(progressBar.current.value) + 5;
+    changeRange();
+  }
+
+  return (
+    <div className={styles.audioPlayer}>
+      <audio ref={audioPlayer} src="https://mp3.chillhop.com/serve.php/?mp3=10075" preload="metadata"></audio>
+      <button className={styles.forwardBackward} onClick={backFive}><BsArrowLeftShort /> 5</button>
+      <button onClick={togglePlayPause} className={styles.playPause}>
+        {isPlaying ? <FaPause /> : <FaPlay className={styles.play} />}
+      </button>
+      <button className={styles.forwardBackward} onClick={forwardFive}>5 <BsArrowRightShort /></button>
+
+      {/* current time */}
+      <div className={styles.currentTime}>{calculateTime(currentTime)}</div>
+
+      {/* progress bar */}
+      <div>
+        <input type="range" className={styles.progressBar} defaultValue="0" ref={progressBar} onChange={changeRange} />
+      </div>
+
+      {/* duration */}
+      <div className={styles.duration}>{(duration && !isNaN(duration)) && calculateTime(duration)}</div>
+    </div>
+  )
 }
-export { AudioPlayer } 
+
+export { AudioPlayer }
